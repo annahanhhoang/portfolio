e0b88d8359cfa3af6d286034d598e389d0f6bc3e
Anna Hoang <anna.hanh.hoang@gmail.com>
2019-03-14 17:14:46 -0500

    landing page layout

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router/router'
+import './styles/main.scss'
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,5 +1,30 @@
 <template>
-    <div>This is landing page</div>
+    <v-content>
+        <v-container fluid fill-height grid-list-sm class="landing-page">
+            <v-layout align-center justify-center>
+                <v-flex xs12 sm8 md4>
+                    <v-form>
+                        <v-layout row wrap>
+                            <v-flex xs3></v-flex>
+                            <v-flex xs6 class="google-logo">
+                            </v-flex>
+                            <v-flex xs3></v-flex>
+
+                            <v-flex xs12 class="input-search">
+                                <v-text-field name="search" type="text" outline hide-details/>
+                            </v-flex>
+                            <v-flex xs6 text-xs-right>
+                                <v-btn>Google Search</v-btn>
+                            </v-flex>
+                            <v-flex xs6>
+                                <v-btn>I'm feeling lucky</v-btn>
+                            </v-flex>
+                        </v-layout>
+                    </v-form>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-content>
 </template>
 
 <script>
