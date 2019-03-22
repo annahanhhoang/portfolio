e96a9a4028b836a9939ef7002da86379ab238cf3
anna_hoang <anna.hanh.hoang@gmail.com>
2019-03-22 16:51:11 -0500

    about page layout

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Landing from '../views/Landing.vue'
+import Landing from '@/views/Landing.vue'
+import About from '@/views/About.vue'
 Vue.use(Router)
 
 const router = new Router({
@@ -10,6 +11,12 @@ const router = new Router({
             name: 'Landing',
             component: Landing,
             meta: { title: 'Welcome to Anna\'s page'},
+        },
+        {
+            path: '/about',
+            name: 'AboutMe',
+            component: About,
+            meta: { title: 'About Me' },
         },
     ]
 })
---
+++ src/views/About.vue
@@ -0,0 +1,94 @@
+<template>
+    <v-layout row class="about-page">
+        <v-flex xs2 class="side-menu">
+            <side-menu></side-menu>
+        </v-flex>
+        <v-flex xs7 class="about-content">
+            <v-layout row wrap>
+                <v-flex xs8>
+                    <h1>Who I am.</h1>
+                    <div>I am a full-stack developer that specializes in Vue and Veutify development. </div>
+                </v-flex>
+                <v-flex xs4>
+                    <v-layout row wrap>
+                        <v-flex xs4 class="label">
+                            LOCATION
+                        </v-flex>
+                        <v-flex xs8>Dallas, Texas</v-flex>
+
+                        <v-flex xs4 class="label">
+                            EMAIL
+                        </v-flex>
+                        <v-flex xs8>anna.hanh.hoang@gmail.com</v-flex>
+
+                        <v-flex xs4 class="label">
+                            PHONE
+                        </v-flex>
+                        <v-flex xs8>469-831-0504</v-flex>
+
+                        <v-flex xs4 class="label">
+                            SKYPE
+                        </v-flex>
+                        <v-flex xs8>hanh.v.i.p</v-flex>
+                    </v-layout>
+                </v-flex>
+
+                <v-flex xs12>
+                    <h1>What I can do for you.</h1>
+                    <v-expansion-panel>
+                        <v-expansion-panel-content>
+                            <template v-slot:header>
+                                <div>Web Development</div>
+                            </template>
+                            <v-card>
+                                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+                            </v-card>
+                        </v-expansion-panel-content>
+                        <v-expansion-panel-content>
+                            <template v-slot:header>
+                                <div>Application Development</div>
+                            </template>
+                            <v-card>
+                                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+                            </v-card>
+                        </v-expansion-panel-content>
+                        <v-expansion-panel-content>
+                            <template v-slot:header>
+                                <div>Mobile Development</div>
+                            </template>
+                            <v-card>
+                                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+                            </v-card>
+                        </v-expansion-panel-content>
+                        <v-expansion-panel-content>
+                            <template v-slot:header>
+                                <div>Design</div>
+                            </template>
+                            <v-card>
+                                <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</v-card-text>
+                            </v-card>
+                        </v-expansion-panel-content>
+                    </v-expansion-panel>
+                </v-flex>
+            </v-layout>
+        </v-flex>
+        <v-flex xs3 class="side-image">
+            <div class="text-xs-right">
+                <div>FULL-STACK DEVELOPER</div>
+                <div>Anna Hoang</div>
+            </div>
+            <div class="side-image--img">
+            </div>
+        </v-flex>
+    </v-layout>
+</template>
+
+<script>
+    import SideMenu from '@/components/SideMenu'
+
+    export default {
+        components: {
+            SideMenu
+        }
+    }
+</script>
