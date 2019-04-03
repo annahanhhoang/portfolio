a1c4d0acc109581184479b626f82351addba311d
anna_hoang <anna.hanh.hoang@gmail.com>
2019-04-03 16:02:34 -0500

    clear console on hot module reload

--- src/main.js
+++ src/main.js
@@ -10,3 +10,9 @@ new Vue({
     router,
     render: h => h(App),
 }).$mount('#app')
+
+if (module.hot) {
+    module.hot.addStatusHandler(status => {
+        if (status === 'prepare') console.clear()
+    })
+}
