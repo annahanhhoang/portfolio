204c328741280793d698afff1a6558e6e32e9917
FNC\AHOANG <anna.hoang@us.fujitsu.com>
2019-04-18 19:32:59 -0500

    social link change
    - add email and phone number
    - add property to control size

--- src/components/SocialLink.vue
+++ src/components/SocialLink.vue
@@ -1,11 +1,9 @@
 <template>
-    <v-layout row class="social-link">
-        <div class="mx-1" v-for="item in accounts" :key="item.id">
-            <a :href="item.link">
-                <v-icon>{{item.icon}}</v-icon>
-            </a>
-        </div>
-    </v-layout>
+    <div class="social-link mx-2">
+        <a v-for="item in accounts" :key="item.id" :href=" item.link">
+            <v-icon :x-large="xl">{{item.icon}}</v-icon>
+        </a>
+    </div>
 </template>
 
 <script>
@@ -16,8 +14,16 @@
                     { id: 'LinkedIn', icon: 'mdi-linkedin-box', link: 'https://www.linkedin.com/in/hanh-hoang-67023654/' },
                     { id: 'GitHub', icon: 'mdi-github-box', link: 'https://github.com/annahanhhoang/' },
                     { id: 'Facebook', icon: 'mdi-facebook-box', link: '#' },
+                    { id: 'Email', icon: 'mdi-email', link: 'mailto:anna.hanh.hoang@gmail.com' },
+                    { id: 'Phone', icon: 'mdi-phone', link: 'tel:+1-469-831-0504' },
                 ],
             }
-        }
+        },
+        props: {
+            xl: {
+                type: Boolean,
+                default: false
+            }
+        },
     }
 </script>
