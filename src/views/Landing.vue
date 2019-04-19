4eeebe13a4b1378e40c87c651aea9ff1e5e9e5cd
FNC\AHOANG <anna.hoang@us.fujitsu.com>
2019-04-18 21:45:44 -0500

    fix landing page button position

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,6 +1,5 @@
 <template>
-    <v-container fluid fill-height grid-list-xl class="landing-page">
-        <v-layout align-center justify-center>
+        <v-layout fill-height align-center justify-center class="landing-page">
             <v-flex xs12 sm8 md4>
                 <v-layout row wrap>
                     <v-flex xs12 md6 offset-md3 class="google-logo">
@@ -11,7 +10,7 @@
                         <v-text-field id="txtSearch" ref="txtSearch" type="text" outline hide-details disabled/>
                         <div class="search-text anim-typewriter">Anna Hoang full stack developer</div>
                     </v-flex>
-                    <v-flex md6 class="hidden-sm-and-down">
+                    <v-flex md6 class="hidden-sm-and-down text-md-right">
                         <v-btn>Google Search</v-btn>
                     </v-flex>
                     <v-flex md6 class="hidden-sm-and-down">
@@ -20,7 +19,6 @@
                 </v-layout>
             </v-flex>
         </v-layout>
-    </v-container>
 </template>
 
 <script>
